e0c5cc9944e16448055217a0c7c7b50d4f8dba12
Tamaki <Tamaki@VAIO>
2026-01-15 02:27:21 +0900

    Java導入

---
+++ HelloWorld_KoizumiTamaki/src/sample/SelfIntroduction.java
@@ -0,0 +1,23 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		// 氏名
+		String name = "小泉　珠紀";
+		// 年齢
+		int age = 25;
+		// 趣味
+		String hobby = "編み物";
+		
+		/* 
+		 * 自己紹介を出力する処理
+		 */
+
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+
+	}
+
+}

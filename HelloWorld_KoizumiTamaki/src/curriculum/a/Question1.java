914a9dec962729658e404e0ed74e1219e86b2105
Tamaki <Tamaki@VAIO>
2026-01-17 22:53:27 +0900

    Question1,2

---
+++ HelloWorld_KoizumiTamaki/src/curriculum/a/Question1.java
@@ -0,0 +1,59 @@
+package curriculum.a;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * [概要] 変数の宣言・初期値の設定、代入処理
+         * [詳細] 各データ型の変数を宣言、初期値を設定し、その後代入する。
+		 */
+		
+		//Question1
+		 
+		byte byteNum = 12;// 変数byteNumを宣言し、125を初期値に設定
+		
+		short shortNum = 125;// 変数shortNumを宣言し、125を初期値に設定
+		
+		int intNum = 1250;// 変数intNumを宣言し、1250を初期値に設定
+		
+		long longNum = 12500;// 変数longNumを宣言し、12500を初期値に設定
+		
+		float floatNum = 2026;// 変数floatNumを宣言し、2026を初期値に設定
+		
+		double doubleNum = 3.14;// 変数doubleNumを宣言し、3.14を初期値に設定
+		
+		char letter = 'W';// 変数letterを宣言し、Wを初期値に設定
+		
+		String letters = "Java";// 変数lettersを宣言し、Javaを初期値に設定
+		
+		boolean isBoolean = false;//変数isBooleanを宣言し、falseを初期値に設定
+		
+		/*
+		 *[詳細]Question1で設定した初期値に新たな数値を代入する。
+		 */
+		
+		// Question2	
+		
+		byteNum = 10; // 変数の値を更新
+		
+		shortNum = 100; // 変数の値を更新
+		
+		intNum = 1000; // 変数の値を更新
+		
+		longNum = 10000; // 変数の値を更新
+		
+		floatNum = 9.5f; // 変数の地を更新
+		
+		doubleNum = 10.5; // 変数の値を更新
+		
+		letter = 'a'; // 変数の値を更新
+		
+		letters = "ハロー"; // 変数の値を更新
+		
+		isBoolean = true; //変数の値を更新 
+		
+
+	}
+
+}

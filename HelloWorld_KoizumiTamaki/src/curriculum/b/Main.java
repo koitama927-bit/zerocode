a2828d119bf0a8cd2b2f310885edc26a56bf3bc7
Tamaki <Tamaki@VAIO>
2026-02-06 09:50:26 +0900

    Question5,Q4,Q5コメントの修正

--- HelloWorld_KoizumiTamaki/src/curriculum/b/Main.java
+++ HelloWorld_KoizumiTamaki/src/curriculum/b/Main.java
@@ -15,15 +15,15 @@ public class Main {
 	// Q5
 	
 	
-	Animal animal = new Animal() ;
+	Animal animal = new Animal() ; // animalインスタンスを作成
 	
-	animal.setName ( "ライオン" );
-	animal.setLength( 2.1 );
-	animal.setSpeed( 80 );
+	animal.setName ( "ライオン" ); // setNameに“ライオン”を設定
+	animal.setLength( 2.1 ); // setLengthに2.1を設定
+	animal.setSpeed( 80 ); // setSpeedに80を設定
 	
-	 System.out.println("動物名: " + animal.getName() );
-     System.out.println("体長: " + animal.getLength() + "m");
-     System.out.println("速度: " + animal.getSpeed() + "km/h");
+	 System.out.println("動物名: " + animal.getName() ); // 動物名を表示
+     System.out.println("体長: " + animal.getLength() + "m"); // 体長を表示
+     System.out.println("速度: " + animal.getSpeed() + "km/h"); // 速度を表示
     
 	
 	}
--- HelloWorld_KoizumiTamaki/src/q5/Animal.java
+++ HelloWorld_KoizumiTamaki/src/q5/Animal.java
@@ -4,29 +4,29 @@ public class Animal {
 	
 	private String name;
 	private double length;
-	private int speed;
+	private int speed; // 動物名、体長、速度それぞれのフィールドを作成
 	
 	public String getName() {
-	    return this.name ;
+	    return this.name ; // 動物名のgetterを作成
 	}
     public void setName( String name ) {
-        this.name = name ;
+        this.name = name ; // thisを使い動物名のsetterを作成
     }
 
     
 	public double getLength() {
-	    return this.length ;
+	    return this.length ; // 体長のgetterを作成
 	}
     public void setLength( double length ) {
-        this.length = length ;
+        this.length = length ; // thisを使い体長のsetterを作成
     }
     
     
     public int getSpeed() {
-    	return this.speed ;
+    	return this.speed ; // 速度のgetterを作成
     }
     public void setSpeed( int speed ) {
-    	this.speed = speed ;
+    	this.speed = speed ; // thisを使い速度のsetterを作成
     }
 
    

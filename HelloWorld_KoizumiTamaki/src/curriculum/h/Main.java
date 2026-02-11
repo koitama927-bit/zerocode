b4a778b7a20d3675ff9e2946a51dcca2ae9fdbd6
Tamaki <Tamaki@VAIO>
2026-02-12 00:33:13 +0900

    コメントの修正

--- HelloWorld_KoizumiTamaki/src/curriculum/h/Main.java
+++ HelloWorld_KoizumiTamaki/src/curriculum/h/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
 		Employee full = new FullTimeEmployee("E001", "佐藤太郎");
 
-		Employee part = new PartTimeEmployee("E002", "田中花子"); // コンストラクタの初期化（指定がなかったため空）
+		Employee part = new PartTimeEmployee("E002", "田中花子"); // コンストラクタの初期化
 
 		System.out.println("正社員の給与:" + full.calculateDailyWage(9) + "円");
 		System.out.println("パート社員の給与:" + part.calculateDailyWage(9) + "円");
